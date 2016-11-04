4495afb2b2225c2f0a8ce235aacead944b93ac70
turin <turin@turins-mac-mini.local>
2016-11-04 06:09:24 -0600

    mini reto terminado

--- Hamburguesas/Hamburguesas/Datos.swift
+++ Hamburguesas/Hamburguesas/Datos.swift
@@ -1,2 +1,45 @@
 import Foundation
+import UIKit
 
+
+class ColeccionDePaises {
+    let paises : [String] = ["Mexico", "Brazil", "Argentina", "Uruguay", "Chile",
+        "España", "Alemania", "Holanda", "Francia", "Inglaterra",
+        "China", "Japon", "Corea del Sur", "Arabia Saudita", "India",
+        "Nigeria", "Egipto", "Sudafrica", "Camerun", "Tunez"]
+    
+    
+    func obtenPais() -> String {
+        let posicion = Int( arc4random() ) % paises.count
+        
+        return paises[posicion]
+    }
+}
+
+class ColeccionDeHamburguesa {
+    let hamburguesas : [String] = ["Hamburguesa Sencilla", "Hamburguesa doble carne", "Hamburguesa pollo", "Hamburguesa vegetariana", "Hamburguesa a la parrilla",
+        "Hamburguesa de la casa", "Hamburguesa con doble queso", "Hamburguesa con arrachera", "Hamburguesa con pescado", "Hamburguesa cubana",
+        "Hamburguesa light", "Hamburguesa con tocino", "Hamburguesa con camaron", "Hamburguesa con salsa bbq", "Hamburguesa al carbon",
+        "Hamburguesa gigante", "Hamburguesa jamon y tocino", "Hamburguesa", "Hamburguesa doble", "Hamburguesa con papas"]
+    
+    func obtenHamburguesa() -> String {
+        let posicion = Int( arc4random() ) % hamburguesas.count
+        
+        return hamburguesas[posicion]
+    }
+}
+
+class ColeccionDeColores {
+    let colores = [UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1),
+        UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1),
+        UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 1),
+        UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1),
+        UIColor(red: 255/255, green: 0/255, blue: 255/255, alpha: 1),
+        UIColor(red: 0/255, green: 255/255, blue: 255/255, alpha: 1)]
+    
+    func obtencionColor() -> UIColor {
+        let posicion = Int( arc4random() ) % colores.count
+        
+        return colores[posicion]
+    }
+}
--- Hamburguesas/Hamburguesas/ViewController.swift
+++ Hamburguesas/Hamburguesas/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var paises = ColeccionDePaises()
+    var hamburguesas = ColeccionDeHamburguesa()
+    var color = ColeccionDeColores()
+    
+    @IBOutlet weak var lblPais: UILabel!
+    @IBOutlet weak var lblHamburguesa: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func cambios() {
+        view.backgroundColor = color.obtencionColor()
+        
+        lblPais.text = paises.obtenPais()
+        lblHamburguesa.text = hamburguesas.obtenHamburguesa()
+    }
 
 }
 
